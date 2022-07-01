8441d4036cf2da868bc50dad06dd47055249058e
DomGibson <103586347+DomGibson@users.noreply.github.com>
2022-07-01 09:07:56 +0100

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -14,7 +14,7 @@ function Post() {
       fetchPhotos();
     }, [])
 
-    const localhost = `http://localhost:${env.PORT}/posts`
+    const localhost = `https://insta-restapi.herokuapp.com/posts`
     const fetchPhotos = async () => {
       const res = await fetch(localhost);
       const data = await res.json();
